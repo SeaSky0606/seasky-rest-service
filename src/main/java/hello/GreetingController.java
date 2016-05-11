ee44df50081988471be141b28c1f8530b709ba12
junhong <847421525@qq.com>
2016-05-11 14:29:44 +0800

    this is the simple template from web restful api which is deployed on tomcat.

---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,35 @@
+package hello;
+
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+import hello.model.FruitModel;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+
+    @RequestMapping("/getFruits")
+    public Map<String,Object> getFruits(@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+        return new FruitModel().getFruits();
+    }
+
+    @RequestMapping("/getFruitByTag")
+    public Map<String, Object> getFruitByTag(@RequestParam(value = "tag", defaultValue = "World") String tag) {
+        return new FruitModel().getFruitByTag(tag);
+    }
+
+
+}
---
+++ src/main/java/hello/GreetingControllerTest.java
@@ -0,0 +1,36 @@
+package hello;
+
+import hello.model.FruitModel;
+import org.springframework.util.Assert;
+
+import javax.validation.constraints.AssertTrue;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Administrator on 2016/5/11.
+ */
+public class GreetingControllerTest {
+    FruitModel model = new FruitModel();
+    GreetingController controller = new GreetingController();
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGreeting() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetFruits() throws Exception {
+        Assert.isTrue(controller.getFruits(null).size() > 0);
+    }
+
+    @org.junit.Test
+    public void testGetFruitByTag() throws Exception {
+        org.junit.Assert.assertEquals("苹果",controller.getFruitByTag("apple").get("fruit"));
+    }
+}
